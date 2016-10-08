fe495176a8028a9e7f86d8ad11e76d67c8a5fc91
Martin Sarzhovski <martin_topansko@hotmail.com>
2016-10-08 22:10:53 +0200

    Add project files.

---
+++ CoffePlanner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoffePlanner.Startup))]
+namespace CoffePlanner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
